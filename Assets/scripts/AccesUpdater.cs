861bde80aef839b2c7495b02f3df6e42450f7a5a
Joaquin Gebauer <joaquin_gebauer@hotmail.com>
2017-08-31 23:30:50 -0300

    agregado script para aumentar el nivel de acceso del jugador

---
+++ Assets/scripts/AccesUpdater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccesUpdater : MonoBehaviour, Usable {
+
+	public void beingUsed (){
+		GameObject.FindObjectOfType<Inventory> ().upgradeAccesLevel ();
+		Destroy (this);
+	}
+}
