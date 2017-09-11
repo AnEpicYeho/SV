6908267c9145bc6fbcc1d8937bdf8fabe92e3acf
Joaquin Gebauer <joaquin_gebauer@hotmail.com>
2017-09-11 11:31:10 -0300

    cambiado nombre clase enemigo

--- Assets/scripts/EnemyMelee.cs
+++ Assets/scripts/EnemyMelee.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour, Damageable {
+public class EnemyMelee : MonoBehaviour, Damageable {
 
 	private float healt = 100;
 	public float armor;
